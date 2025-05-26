7cc404ed3f4a0a682922e796518330f8532144d0
shashankp10-TTN <shashank.pandey@tothenew.com>
2025-05-26 15:49:45 +0530

    feat : enabled logging

--- producer/src/main/java/com/order_service_poc/producer/ProducerApplication.java
+++ producer/src/main/java/com/order_service_poc/producer/ProducerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProducerApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("javax.net.debug", "ssl:handshake");
 		SpringApplication.run(ProducerApplication.class, args);
 	}
 
