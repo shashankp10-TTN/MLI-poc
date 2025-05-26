059f959b5a902ea77868f55e168f4f18cfaeb6c0
shashankp10-TTN <shashank.pandey@tothenew.com>
2025-05-26 14:35:42 +0530

    feat : enabled ssl

--- producer/src/main/java/com/order_service_poc/producer/config/AppConfig.java
+++ producer/src/main/java/com/order_service_poc/producer/config/AppConfig.java
@@ -1,14 +1,61 @@
 package com.order_service_poc.producer.config;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyStore;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${server.ssl.key-store}")
+    private Resource keyStore;
+
+    @Value("${server.ssl.key-store-password}")
+    private String keyStorePassword;
+
+    @Value("${server.ssl.trust-store}")
+    private Resource trustStore;
+
+    @Value("${server.ssl.trust-store-password}")
+    private String trustStorePassword;
+
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate() throws Exception {
+        KeyStore keyStoreInstance = KeyStore.getInstance("PKCS12");
+        keyStoreInstance.load(keyStore.getInputStream(), keyStorePassword.toCharArray());
+
+//        KeyStore trustStoreInstance = KeyStore.getInstance("PKCS12");
+//        trustStoreInstance.load(trustStore.getInputStream(), trustStorePassword.toCharArray());
+
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadKeyMaterial(keyStoreInstance, keyStorePassword.toCharArray())
+                .loadTrustMaterial(keyStoreInstance, null)
+                .build();
+
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+
+        HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .setSSLSocketFactory(socketFactory)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setHttpClient(httpClient);
+        return new RestTemplate(factory);
     }
 }
